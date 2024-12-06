89f3f943d5067c7dc9436fc045e00bd970ad0673
klausdev <klausdev@users-MacBook-Pro.local>
2024-12-06 12:47:15 +0100

    done

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,45 @@
 import express from "express"
+import User from "../models/userModel.js";
+import bcrypt from "bcryptjs"
 
-export const registerUser = (req, res) => {
-    return res.status(201).json({
-        message:"user is regissted"
+export const registerUser = async (req, res) => {
+   const {firstName, lastName, email, phone, password} = req.body; // got input ffrom the user
+
+   //validation of input
+   if(!firstName || !lastName || !email || !phone || !password){
+    return res.status(400).json({
+        message:"please fill all the required fields"
     })
+   }
+
+   //check for old users
+   const oldUser = await User.findOne({email})
+   if(oldUser){
+    return res.status(409).json({
+        message:"Email already exist"
+    })
+   }
+
+   const salt = await bcrypt.genSalt()
+
+   //enrypt my password
+   const hashedPassword = await bcrypt.hash(password, salt)
+   //send email to my user
+   //validate
+
+   const newUser = await User.create({
+    firstName,
+    lastName,
+    email,
+    password:hashedPassword,
+    phone
+   })
+
+   return res.status(201).json({
+    message:"user created successfully",
+    newUser
+
+   })
 }
 
 export const loginUser= (req, res) => {
